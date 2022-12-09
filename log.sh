70b2a61f368034c7f538cbf8177fa422b09e4182
Marechal <Marechal.Moore@outlook.com>
2022-12-08 20:12:19 -0500

    update

---
+++ log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Marechal Moore
+# Date of latest revision:
+# Purpose: Print a string to the terminal
+
+echo "Here is my log history"
+
+function loghistory(){
+last 
+};
+loghistory
